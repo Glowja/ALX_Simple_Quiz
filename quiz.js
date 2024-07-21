7829a45c029f53b9dce479f9dade89df7eeef661
Glowja <gloryjibril433@gmail.com>
2024-07-21 23:07:59 +0100

    commit

--- quiz.js
+++ quiz.js
@@ -11,3 +11,9 @@ let value =["userAnswer"]
 value.forEach(value => {
     console.log
 });
+let correctAnswer = 4().userAnswer();
+if (value > or < 4) {
+  feedback = "correct! Weldone done";
+} else {
+  feedback = "That's incorrect. Try again!";
+}
