874859479ff40480cbc0c4ceb9ff31acb3479bbd
Glowja <gloryjibril433@gmail.com>
2024-07-21 21:28:07 +0100

    enter

--- quiz.js
+++ quiz.js
@@ -1,7 +1,10 @@
 function checkAnswer() {
     // Function body
     let correctAnswer =('4')
+    return 2+2
     console.log('correctAnswer')
 }
 document.querySelector("quiz")
 Object.values['4']
+getElementById ("choice1")
+let value =(userNameS)
